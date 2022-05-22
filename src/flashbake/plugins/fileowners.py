1539d4e2ed52cd9106223f88684b2d63b8483133
Ian Paul <ian@ianpaul.net>
2022-05-22 10:15:01 +0000

    File owners works as expected now.

--- src/flashbake/plugins/fileowners.py
+++ src/flashbake/plugins/fileowners.py
@@ -2,19 +2,29 @@
 
 from flashbake.plugins import AbstractMessagePlugin
 import subprocess
+import os.path
 
-class Ownership(AbstractMessagePlugin):
+class FileOwners(AbstractMessagePlugin):
     def __init__(self, plugin_spec):
-        AbstractMessagePlugin.__init__(self, plugin_spec, True)
-        self.define_property('owners_and_groups', str, False, default='.')
+        AbstractMessagePlugin.__init__(self, plugin_spec, False)
+        self.define_property('owners', required=False)
 
     def addcontext(self, message_file, config):
-        ''' Add owners and groups data for the files and folders in the current directory. '''
-        check = subprocess.run(["ls", "-lA", self.owners_and_groups], capture_output=True, text=True).stdout.strip("\n")
-        for line in check.splitlines()[1:]:
-            fields = line.split()
-            message_file.write("{0} {1} {2}\n".format(fields[2], fields[3], fields[8])) 
-        if self.owners_and_groups == None:
+        ''' If the owners variable is not present in the config. Write an error message to the config. '''
+        if self.owners == None:
             message_file.write('Couldn\'t find the specified directory. Please ensure the config uses an absolute path.')
             return False
+
+        ''' Add owners and groups data for the files and folders in the current directory. '''
+        fields = self.__getowners(self.owners)
+        for i in range(len(fields)):
+            message_file.write("{0} {1} {2}\n".format(fields[i][2], fields[i][3], fields[i][8]))
+
+    def __getowners(self, owners):
+        check = subprocess.run(["ls", "-lA", owners], capture_output=True, text=True).stdout.strip("\n")
+        fields = []
+        for line in check.splitlines()[1:]:
+            x = line.split()
+            fields.append(x)
+        return fields 
 
